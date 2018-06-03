a581602fff07b1e60c456223c56c9b6d8d429981
balogh.roland <balogh.roland33@gmail.com>
2018-06-03 16:17:34 +0200

    add privacy policy to website

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,7 @@ exports = module.exports = function (app) {
 	app.get('/araink', routes.views.prices);
 	app.get('/statisztika', routes.views.stats);
 	app.get('/tajekoztato', routes.views.info);
+	app.get('/adatvedelem', routes.views.data_privacy);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
