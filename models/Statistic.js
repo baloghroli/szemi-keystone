b333ccbb85f71ed3df12b18be156e213224892d0
Balogh Roland <roland.balogh@guidesmiths.com>
2019-01-28 18:29:44 +0100

    model modifications

--- models/Statistic.js
+++ models/Statistic.js
@@ -9,7 +9,7 @@ var Statistic = new keystone.List('Statistic');
 
 Statistic.add({
 	mutato: { type: Types.Select, options: 'ÁKÓ, VSM, KK', initial: true },
-	tipus: { type: Types.Select, options: 'A, A1, A és A1, A-A1-A2, AM, B, C' },
+	tipus: { type: Types.Select, options: 'A, A1, A2, A-A1-A2, AM, B, C' },
 	elmelet: { type: String },
 	gyakorlat: { type: String },
 	koltseg: { type: String },
